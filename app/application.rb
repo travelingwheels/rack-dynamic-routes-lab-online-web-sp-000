018daa3b88f14c930cdbeb4e9f851286372a2545
Gary Wheeler <gkwheeler40@gmail.com>
2020-03-03 17:52:34 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -9,11 +9,15 @@ class Application
       item_name = req.path.split(/items/).last
       if item = @@items.find{|i| i.name = item_name}
       resp.write item.price
-    end
+    else 
+        resp.status = 400
+        resp.write "Item not found"
+      end
+    else
+      resp.status=404
       resp.write "Route not found"
-      resp.status = 404
     end
- 
     resp.finish
   end
+
 end
