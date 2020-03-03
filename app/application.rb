f41f40000975ea88a159a6b3c11cc75f0b2e7bb3
Gary Wheeler <gkwheeler40@gmail.com>
2020-03-03 17:57:25 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -9,9 +9,10 @@ class Application
       item_name = req.path.split("/items/").last
       if item = @@items.find{|i| i.name = item_name}
       resp.write item.price
-    else 
+    else
+        
         resp.status = 400
-        resp.write "Item not found"
+        r
       end
     else
       resp.status=404
